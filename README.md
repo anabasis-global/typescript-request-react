24887cf83f41238874d2185c66b73dcf0dab82bc
anabasis-global <136603493+anabasis-global@users.noreply.github.com>
2023-10-03 02:49:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-request-react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-dqjama)
---
+++ index.ts
@@ -0,0 +1,18 @@
+import { formToJSON } from 'axios';
+import { typeFromAST } from 'graphql/utilities/typeFromAST';
+import React from 'react'
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+
+let data = [{key: 2},{key:3}]
+
+
+// appDiv.innerHTML = data.flatMap(i => React.createElement('div', i))
+
+// formToJSON
+
+
+let container = appDiv
