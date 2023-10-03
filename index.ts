9f1cf0f0acf3e48d4bc0a2ace482c19d8725ae71
anabasis-global <136603493+anabasis-global@users.noreply.github.com>
2023-10-03 08:00:51 +0300

    3 files modified

--- index.ts
+++ index.ts
@@ -1,18 +1,42 @@
 import { formToJSON } from 'axios';
 import { typeFromAST } from 'graphql/utilities/typeFromAST';
-import React from 'react'
+import React from 'react';
+import { createPortal, render } from 'react-dom';
+
+import { createRoot, hydrateRoot } from 'react-dom/client';
+
+import { renderToString } from 'react-dom/server';
 // Import stylesheets
 import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 
-let data = [{key: 2},{key:3}]
-
+let data = [
+  {
+    id: 1,
+    key: 1,
+    name: 'VZ',
+    color: 'orange',
+  },
+  {
+    id: 2,
+    key: 2,
+    name: 'KS',
+    color: 'red',
+  },
+];
 
 // appDiv.innerHTML = data.flatMap(i => React.createElement('div', i))
 
 // formToJSON
 
+let root = createRoot(appDiv!);
 
-let container = appDiv
+root.render(
+ `<div>1</div>`
+);
+// let ssr = renderToString(<div>iten<div/>)
+
+// console.log(ssr)
+// let container = appDiv
